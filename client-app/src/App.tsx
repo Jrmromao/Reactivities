7a4afb96b1e861b8b73f1f182ba21883a8fcf0c3
JO20072605 <joao.romao@wipro.com>
2019-11-27 13:23:45 +0000

    using semantic-ui-react to develop the frontend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,39 +1,40 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import axios from 'axios';
+// import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
+import { Header, Icon, List } from "semantic-ui-react";
 
 // react functional compo
 class App extends Component {
-  
   state = {
     values: []
   };
 
   componentDidMount() {
-    
     // promess based API
-    axios.get('http://localhost:5000/api/values')
-      .then((response) => {
-        console.log(response)
-        this.setState({
-          values:  response.data
-          });
-      })
-      
+    axios.get("http://localhost:5000/api/values").then(response => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      });
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-              <ul>
-                  {this.state.values.map((value: any) => (
-                      <li key={value.id}>{value.name}</li>
-                  ))}
-              </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+
+        <List>
+        
+          {this.state.values.map((value: any) => (
+          
+          <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+  </List>
       </div>
     );
   }
