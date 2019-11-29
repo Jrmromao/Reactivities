3ba6838d9909104c68789914df6ac6214d4babf0
JO20072605 <joao.romao@wipro.com>
2019-11-29 15:46:08 +0000

    CDUD functionality completed

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,38 +1,79 @@
-import React, { Component, useState, useEffect } from "react";
+import React, {  useState, useEffect, Fragment } from "react";
 // import logo from "./logo.svg";
 import axios from "axios";
-import { Header, Icon, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
-
-// interface IState{
-//   activities: IActivity[]
-// }
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 // react functional compo
+
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectedActivity = (id: string) => {
+    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  const handleCreateActicity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleDeleteActivity = (id: string ) => {
+    setActivities([...activities.filter(a =>a.id !== id)]);
+  }
+
   useEffect(() => {
     // promess based API. bnow we know that our request is getting an IActivity type
     axios
       .get<IActivity[]>("http://localhost:5000/api/activities")
       .then(response => {
+        let activities: IActivity[] = [];
+        response.data.forEach(activity => {
+          activity.date = activity.date.split('.')[0];
+          activities.push(activity);
+        })
+
         setActivities(response.data);
       });
   }, []); // add this empty array to make sure the useEffect method is not called again.
 
   return (
-    <div>
-      
-      <NavBar/>>
+    <Fragment>
+      <NavBar openCreateForm={handleOpenCreateForm} />
 
-      <List>
-        {/* i could not see anything in the broweser because i'm not using strong type, ie activity: any */}
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectedActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActicity={handleCreateActicity}
+          editActivity={handleEditActivity}
+          deleteActivity = {handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
   );
 };
 
